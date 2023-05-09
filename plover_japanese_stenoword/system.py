a3cf0a3a5167afcda4f64861440b5b0d5d0f0476
sa8kmc <programmed.heim@gmail.com>
2023-05-09 20:10:55 +0900

    bug fix, respond to

--- plover_japanese_stenoword/system.py
+++ plover_japanese_stenoword/system.py
@@ -1,10 +1,11 @@
 # LM NR PTHKS UAOIE *
 KEYS = (
+    '#',
+    'P-', 'T-', 'H-', 'K-', 'S-',
     'L-', 'M-',
     '-N', '-R',
-    'P-', 'T-', 'H-', 'K-', 'S-',
     '-U', '-A', '-O', '-I', '-E',
-    '#'
+    '$', '1', '2', '3', '4'
 )
 
 IMPLICIT_HYPHEN_KEYS = KEYS
@@ -15,7 +16,7 @@ NUMBER_KEY = None
 
 NUMBERS = {}
 
-UNDO_STROKE_STENO = '#'
+UNDO_STROKE_STENO = '#$'
 
 ORTHOGRAPHY_RULES = []
 
@@ -27,19 +28,24 @@ KEYMAPS = {
     'TX Bolt': {
         '#': '#',
         'P-': 'S-',
-        'T-': ('T-', 'K-'),
-        'H-': ('P-', 'W-'),
-        'K-': ('H-','R-'),
+        'T-': 'T-',
+        'H-': 'P-',
+        'K-': 'H-',
         'S-': '*',
         'L-': 'A-',
         'M-': 'O-',
         '-N': '-E',
         '-R': '-U',
-        '-U': ('-F','-R'),
-        '-A': ('-P','-B'),
-        '-O': ('-L','-G'),
-        '-I': ('-T','-S'),
-        '-E': ('-D','-Z'),
+        '-U': '-F',
+        '-A': '-P',
+        '-O': '-L',
+        '-I': '-T',
+        '-E': '-D',
+        '$': '-Z',
+        '1': 'K-',
+        '2': 'W-',
+        '3': '-R',
+        '4': '-B',
     },
     'Keyboard': {
         '#': ('q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'),
@@ -59,7 +65,7 @@ KEYMAPS = {
         '-E': ';',
         'arpeggiate': 'space',
         # Suppress adjacent keys to prevent miss-strokes.
-        'no-op'     : ('z', 'x', 'b', ',', '.', '/'),
+        'no-op': ('z', 'x', 'b', ',', '.', '/'),
     },
 }
 
@@ -69,4 +75,3 @@ DEFAULT_DICTIONARIES = (
     'commands.json',
     'StenoWord.json',
 )
-
